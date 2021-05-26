fce2aa6d1e56e8d283c7a51f8828715346058e20
Andrew Chen <chenandrew@google.com>
2021-05-26 11:16:49 -0700

    add example of tensorboard reading cifar10 tensorflow logs
    PiperOrigin-RevId: 375987832
    Change-Id: I2fd2685ec106b80d821f9245f1832e6e75e735e3
    GitOrigin-RevId: e0801b0be83a38d3a0a934d7f3e4f60bbaa37c86

---
+++ examples/cifar10_tensorflow/launcher.py
@@ -0,0 +1,73 @@
+# Copyright 2021 DeepMind Technologies Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+r"""XManager launcher for CIFAR10.
+
+Usage:
+
+xmanager launch examples/cifar10_tensorflow/launcher.py -- \
+  [--image_path=gcr.io/path/to/image/tag]
+"""
+import asyncio
+import itertools
+
+from absl import app
+from absl import flags
+from xmanager import xm
+from xmanager import xm_local
+from xmanager.cloud import caip
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string('tensorboard', None, 'Tensorboard instance.')
+
+
+def main(_):
+  with xm_local.create_experiment(experiment_title='cifar10') as experiment:
+    spec = xm.PythonContainer(
+        # Package the current directory that this script is in.
+        path='.',
+        base_image='gcr.io/deeplearning-platform-release/tf2-gpu.2-1',
+        entrypoint=xm.ModuleName('cifar10'),
+    )
+
+    [executable] = experiment.package([
+        xm.Packageable(
+            executable_spec=spec,
+            executor_spec=xm_local.Caip.Spec(),
+            args={},
+        ),
+    ])
+
+    learning_rates = [0.1, 0.001]
+    trials = list(
+        dict([('learning_rate', lr)])
+        for (lr,) in itertools.product(learning_rates))
+
+    tensorboard = FLAGS.tensorboard
+    if not tensorboard:
+      tensorboard = caip.client().create_tensorboard('cifar10')
+      tensorboard = asyncio.get_event_loop().run_until_complete(tensorboard)
+
+    for hyperparameters in trials:
+      experiment.add(
+          xm.Job(
+              executable=executable,
+              executor=xm_local.Caip(
+                  xm.JobRequirements(),
+                  tensorboard=xm_local.TensorboardCapability(name=tensorboard)),
+              args=hyperparameters,
+          ))
+
+
+if __name__ == '__main__':
+  app.run(main)
