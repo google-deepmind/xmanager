ae7d2c654578d1929cdb70e4c8fcea27d2a7f49c
Dmitry Tsarkov <tsar@google.com>
2021-08-17 06:33:23 -0700

    Merge two upgrade statements in the default Dockerfile.
    PiperOrigin-RevId: 391275865
    Change-Id: Ide78b16afb25b4d5213274abab0ec80aae86e7ea
    GitOrigin-RevId: df73221c67beaf1796c4f6d8ba94aa050f0fa89c

--- xmanager/cloud/build_image.py
+++ xmanager/cloud/build_image.py
@@ -171,8 +171,7 @@ def default_steps(directory: str, use_deep_module: bool) -> List[str]:
       # Updating and installing on the same line causes cache-busting.
       # https://docs.docker.com/develop/develop-images/dockerfile_best-practices/#run
       'RUN apt-get update && apt-get install -y git',
-      'RUN python -m pip install --upgrade pip',
-      'RUN python -m pip install --upgrade setuptools',
+      'RUN python -m pip install --upgrade pip setuptools',
       f'COPY {directory}/requirements.txt {project_dir}/requirements.txt',
       f'RUN python -m pip install -r {directory}/requirements.txt',
       # It is best practice to copy the project directory as late as possible,
