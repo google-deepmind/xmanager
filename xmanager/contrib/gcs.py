b361340e48a4baff22ceba7cc96b313a216ec586
Dmitry Tsarkov <tsar@google.com>
2021-08-20 02:20:06 -0700

    Add helpers to work with GCS.
    PiperOrigin-RevId: 391947850
    Change-Id: Iba4a854a68a634b79f3256430fcd4ddaee497911
    GitOrigin-RevId: b3b35586f74f88608eada3c83a9e93f0a995328b

---
+++ xmanager/contrib/gcs.py
@@ -0,0 +1,182 @@
+# Copyright 2021 DeepMind Technologies Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Helpers to support GCS-based output dir.
+
+For the code running on Google Cloud a GCS directory should be used to store
+data.
+
+A GCS name should have a particular format 'gs://bucket/directory'. For XCloud
+we provide a shared bucket which can be used by all researchers. To ensure the
+format of the name is correct, and to suggest reasonable defaults for it, this
+file provides a flag and several helper functions.
+"""
+
+import datetime
+import getpass
+import os
+
+from absl import app
+from absl import flags
+
+_GCS_PATH = flags.DEFINE_string(
+    'gcs_path', None, 'A GCS directory within a bucket to store output '
+    '(in gs://bucket/directory format).')
+
+_GS = 'gs://'
+_GCS = '/gcs/'
+
+
+def suggestion(project_name: str) -> str:
+  """Returns a suggested GCS dir name for the given @project_name."""
+  return os.path.join(
+      _GS, 'xcloud_public_bucket', getpass.getuser(),
+      project_name + '-' + datetime.datetime.now().strftime('%Y%m%d-%H%M%S'))
+
+
+def get_gcs_path_or_suggestion(project_name: str) -> str:
+  """Returns a value passed in the --gcs_path flag or a suggested valid path.
+
+  Args:
+    project_name: a project name used to generate suggested GCS path.
+
+  Returns:
+    If the --gcs_path flag is empty, returns a reasonable suggestion.
+    If the --gcs_path contains invalid value, raise an error.
+    Otherwise, returns a flag value.
+  """
+  if not _GCS_PATH.value:
+    return suggestion(project_name)
+  elif not is_gcs_path(_GCS_PATH.value):
+    raise app.UsageError(
+        '--gcs_path not in gs://bucket/directory or /gcs/path format. ' +
+        f'Suggestion: --gcs_path={suggestion(project_name)}')
+  return str(_GCS_PATH.value)
+
+
+def get_gcs_path_or_fail(project_name: str) -> str:
+  """Returns a value passed in the --gcs_path flag; fails if nothing is passed.
+
+  Args:
+    project_name: a project name used to generate suggested GCS path.
+
+  Returns:
+    If the --gcs_path flag is empty, or contains invalid value, raise an error.
+    Otherwise, returns a flag value.
+  """
+  if not _GCS_PATH.value:
+    raise app.UsageError('--gcs_path is missing. Suggestion: ' +
+                         f'--gcs_path={suggestion(project_name)}')
+  elif not is_gcs_path(_GCS_PATH.value):
+    raise app.UsageError(
+        '--gcs_path not in gs://bucket/directory or /gcs/path format. ' +
+        f'Suggestion: --gcs_path={suggestion(project_name)}')
+  return str(_GCS_PATH.value)
+
+
+def is_gs_path(path: str) -> bool:
+  """Given the path, checks whether it is a valid Google Storage URL.
+
+  Args:
+    path: a path.
+
+  Returns:
+    True iff a path starts with the 'gs://' prefix.
+  """
+  return path.startswith(_GS)
+
+
+def is_gcs_fuse_path(path: str) -> bool:
+  """Given the path, checks whether it is a valid gcs_fuse path.
+
+  Args:
+    path: a path.
+
+  Returns:
+    True iff a path starts with the '/gcs/' prefix.
+  """
+  return path.startswith(_GCS)
+
+
+def is_gcs_path(path: str) -> bool:
+  """Given the path, checks whether it is a valid GCS URL supported by XCloud.
+
+  Args:
+    path: a path.
+
+  Returns:
+    True iff a path starts with either 'gs://' or '/gcs/' prefix.
+  """
+  return is_gs_path(path) or is_gcs_fuse_path(path)
+
+
+def _gcs_path_no_prefix(path: str) -> str:
+  """Given the GCS path in gs://bucket/directory format, strips 'gs://' from it.
+
+  Args:
+    path: GCS path in gs://bucket/directory format.
+
+  Returns:
+    Path without 'gs://' prefix.
+  """
+  if is_gs_path(path):
+    return path[len(_GS):]
+  if is_gcs_fuse_path(path):
+    return path[len(_GCS):]
+  raise ValueError(
+      f'Path not in gs://bucket/directory or /gcs/path format: {path}')
+
+
+# Exposed for testing.
+gcp_website_url = 'https://console.cloud.google.com'
+
+
+def get_gcs_url(path: str) -> str:
+  """Given the GCS path, provides a GCS URL to access it.
+
+  Args:
+    path: GCS path in gs://bucket/directory format.
+
+  Returns:
+    GCS URL to access path.
+  """
+  no_prefix = _gcs_path_no_prefix(path)
+  return f'{gcp_website_url}/storage/browser/{no_prefix}'
+
+
+def get_gcs_fuse_path(path: str) -> str:
+  """Given the GCS path, provides corresponding Cloud Storage FUSE path.
+
+  See https://cloud.google.com/storage/docs/gcs-fuse for details.
+
+  Args:
+    path: GCS path in gs://bucket/directory format.
+
+  Returns:
+    Path in the /gcs/bucket/directory format.
+  """
+  return _GCS + _gcs_path_no_prefix(path)
+
+
+def get_gs_path(path: str) -> str:
+  """Given the GCS path, provides corresponding Cloud Storage URL.
+
+  See https://cloud.google.com/storage/docs/gcs-fuse for details.
+
+  Args:
+    path: GCS path in /gcs/bucket/directory format.
+
+  Returns:
+    Path in the gs://bucket/directory format.
+  """
+  return _GS + _gcs_path_no_prefix(path)
---
+++ xmanager/contrib/gcs_test.py
@@ -0,0 +1,106 @@
+# Copyright 2021 DeepMind Technologies Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for the xmanager.contrib.gcs module."""
+
+import getpass
+
+from absl import app
+from absl.testing import absltest
+from absl.testing import flagsaver
+from absl.testing import parameterized
+from xmanager.contrib import gcs
+
+# Error patterns.
+_GCS_PATH_ERROR = '--gcs_path not in gs://bucket/directory or /gcs/path format.'
+_PATH_ERROR = 'Path not in gs://bucket/directory or /gcs/path format'
+
+
+class GcsTest(parameterized.TestCase):
+
+  def gcs_prefix(self, project_name: str) -> str:
+    return ('gs://xcloud_public_bucket/' + getpass.getuser() + '/' +
+            project_name + '-')
+
+  def test_gcs_path_suggestions(self):
+    self.assertStartsWith(gcs.suggestion('xcloud'), self.gcs_prefix('xcloud'))
+
+  def test_gcs_path_empty_flag(self):
+    self.assertEqual(
+        gcs.get_gcs_path_or_suggestion('xcloud'), gcs.suggestion('xcloud'))
+    with self.assertRaisesRegex(app.UsageError, '--gcs_path is missing'):
+      gcs.get_gcs_path_or_fail('xcloud')
+
+  def test_gcs_path_correct_value(self):
+    with flagsaver.flagsaver(gcs_path='gs://bucket/dir'):
+      self.assertEqual(
+          gcs.get_gcs_path_or_suggestion('xcloud'), 'gs://bucket/dir')
+      self.assertEqual(gcs.get_gcs_path_or_fail('xcloud'), 'gs://bucket/dir')
+
+  def test_gcs_path_incorrect_value(self):
+    with flagsaver.flagsaver(gcs_path='file://dir'):
+      with self.assertRaisesRegex(app.UsageError, _GCS_PATH_ERROR):
+        gcs.get_gcs_path_or_suggestion('xcloud')
+        gcs.get_gcs_path_or_fail('xcloud')
+
+  # pylint: disable=bad-whitespace
+  @parameterized.named_parameters(
+      ('gs_long',      'gs://a/b/c', True,  False, True),
+      ('gs_short',     'gs://d/e',   True,  False, True),
+      ('gs_invalid',   'gs:/d/e',    False, False, False),
+      ('fuse_long',    '/gcs/a/b/c', False, True,  True),
+      ('fuse_short',   '/gcs/d/e',   False, True,  True),
+      ('fuse_invalid', '/gcsc/d/e',  False, False, False),
+      ('invalid',      'a/b/f',      False, False, False),
+  )  # pyformat:disable
+  # pylint: enable=bad-whitespace
+  def test_is_path(self, path, expected_gs, expected_fuse, expected_gcs):
+    self.assertEqual(gcs.is_gs_path(path), expected_gs)
+    self.assertEqual(gcs.is_gcs_fuse_path(path), expected_fuse)
+    self.assertEqual(gcs.is_gcs_path(path), expected_gcs)
+
+  def test_get_gcs_url(self):
+    self.assertEqual(
+        gcs.get_gcs_url('gs://a/b/c'),
+        f'{gcs.gcp_website_url}/storage/browser/a/b/c')
+    self.assertEqual(
+        gcs.get_gcs_url('gs://d/e'),
+        f'{gcs.gcp_website_url}/storage/browser/d/e')
+    self.assertEqual(
+        gcs.get_gcs_url('/gcs/a/b/c'),
+        f'{gcs.gcp_website_url}/storage/browser/a/b/c')
+    self.assertEqual(
+        gcs.get_gcs_url('/gcs/d/e'),
+        f'{gcs.gcp_website_url}/storage/browser/d/e')
+    with self.assertRaisesRegex(ValueError, _PATH_ERROR):
+      gcs.get_gcs_url('a/b/f')
+
+  def test_get_gcs_fuse_path(self):
+    self.assertEqual(gcs.get_gcs_fuse_path('gs://a/b/c'), '/gcs/a/b/c')
+    self.assertEqual(gcs.get_gcs_fuse_path('gs://d/e'), '/gcs/d/e')
+    self.assertEqual(gcs.get_gcs_fuse_path('/gcs/a/b/c'), '/gcs/a/b/c')
+    self.assertEqual(gcs.get_gcs_fuse_path('/gcs/d/e'), '/gcs/d/e')
+    with self.assertRaisesRegex(ValueError, _PATH_ERROR):
+      gcs.get_gcs_fuse_path('a/b/f')
+
+  def test_get_gs_path(self):
+    self.assertEqual(gcs.get_gs_path('gs://a/b/c'), 'gs://a/b/c')
+    self.assertEqual(gcs.get_gs_path('gs://d/e'), 'gs://d/e')
+    self.assertEqual(gcs.get_gs_path('/gcs/a/b/c'), 'gs://a/b/c')
+    self.assertEqual(gcs.get_gs_path('/gcs/d/e'), 'gs://d/e')
+    with self.assertRaisesRegex(ValueError, _PATH_ERROR):
+      gcs.get_gcs_fuse_path('a/b/f')
+
+
+if __name__ == '__main__':
+  absltest.main()
