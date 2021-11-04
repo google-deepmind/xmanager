92f88e171035d870825acfb25c4584297607fcbc
Alexey Guseynov <kibergus@google.com>
2021-11-04 07:26:57 -0700

    Increase version to 0.1.3 for the next PyPI release.
    PiperOrigin-RevId: 407575299
    Change-Id: I36bc172021d9d781aba74b1d45628b79b22f8c6d
    GitOrigin-RevId: efcb16b6f5924a0f7340b16cb7dc2814aff8eee5

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='xmanager',
-    version='0.1.2',
+    version='0.1.3',
     description='A framework for managing machine learning experiments',
     long_description=long_description,
     long_description_content_type='text/markdown',
