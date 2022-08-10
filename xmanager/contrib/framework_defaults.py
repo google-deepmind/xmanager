e34addfba466b7313a32c8e67a4639c7db9b67c4
DeepMind Team <no-reply@deepmind.com>
2022-08-09 17:06:29 -0700

    internal
    PiperOrigin-RevId: 466511989
    Change-Id: I6bfd5aae5a34441c209391a215f6bb5b8b22b223
    GitOrigin-RevId: a556c92cd05efc348b34cfaa3beda53c053b5a31

--- xmanager/contrib/framework_defaults.py
+++ xmanager/contrib/framework_defaults.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """Automatic selection of reasonable defaults based on input.
 
 This library aimed to provide recommended defaults for the choices that can be
