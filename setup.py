e040e768cad572a44700608ea7c05bbd022e1d37
Andrew Chen <chenandrew@google.com>
2023-04-18 06:53:46 -0700

    bump version to 0.4.0
    PiperOrigin-RevId: 525134310
    Change-Id: Ifcf108ee38abcc4488eb54a1d56dbfe490704261
    GitOrigin-RevId: f163726ffcbc614ae6064f5dd2e63668690f185e

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='xmanager',
-    version='0.3.0',
+    version='0.4.0',
     description='A framework for managing machine learning experiments',
     long_description=long_description,
     long_description_content_type='text/markdown',
