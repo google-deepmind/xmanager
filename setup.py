c9c7a46957c052978b578411f3c385e47e663fc5
DeepMind Team <no-reply@deepmind.com>
2025-05-05 03:34:34 -0700

    Internal.
    PiperOrigin-RevId: 754867371
    Change-Id: I64cfb0102ca48533f8dfd072c4983e12ca55b1e0
    GitOrigin-RevId: 1557bd76322c2b97076d3919267dfa12c804d6d7

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='xmanager',
-    version='0.7.0',
+    version='0.7.1',
     description='A framework for managing machine learning experiments',
     long_description=long_description,
     long_description_content_type='text/markdown',
