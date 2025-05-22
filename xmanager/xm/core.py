7a9cfd0b1edf777cd4b10b9f81ab3a8a8bbdc9ec
Alfred Piccioni <alpic@google.com>
2025-05-22 01:55:10 -0700

    Internal
    PiperOrigin-RevId: 761862902
    Change-Id: I1a54be43c50d6aa229689fda01c186dabaddca7c
    GitOrigin-RevId: 5321f94c3aa076cb8fe559ea7f7417ddd2a1c6b5

--- xmanager/xm/core.py
+++ xmanager/xm/core.py
@@ -92,23 +92,6 @@ def _apply_args(job_type: job_blocks.JobType, args: Mapping[str, Any]) -> None:
   # pytype: enable=attribute-error
 
 
-def get_contextvar_value_by_name(name: str) -> Any | None:
-  ctx = contextvars.copy_context()
-  # Note: returns None when the var is unset, even if the ContextVar is defined
-  # with a non-None `default` kwarg.
-  for k, v in ctx.items():
-    if k.name == name:
-      return v
-  return None
-
-
-def get_xid_from_context() -> int:
-  """Returns the XID from the context. Returns -1 if not found."""
-  if experiment := get_contextvar_value_by_name('_xm_current_experiment'):
-    return experiment.experiment_id
-  return -1
-
-
 def _is_coro_context() -> bool:
   """Returns whether we are currently running in a coroutine context."""
   is_coro_context = False
