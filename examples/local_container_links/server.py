dbf0c2577dfe3aeb407cb4b9588eb7867a404857
Kristian Hartikainen <kristian@ekarobotics.com>
2025-06-22 17:29:31 -0400

    Migrate `local_container_links` to `flask`

--- examples/local_container_links/server.py
+++ examples/local_container_links/server.py
@@ -1,32 +1,28 @@
 """An HTTP server incrementing a value in Redis."""
 
+import time
 from typing import Sequence
 
 from absl import app
 from absl import flags
-import bottle
+from flask import Flask
 import redis
 
 redis_host = flags.DEFINE_string("redis_host", None, "Address to Redis server.")
 
-server = bottle.Bottle()
-rdb = None
+server = Flask(__name__)
 
 
 @server.route("/increment")
 def increment():
-  global rdb
-  if rdb is None:
-    return "Redis host not set."
+  rdb = server.config["RDB"]
   counter = rdb.incr("counter")
   return f"{counter=}"
 
 
 @server.route("/")
 def index():
-  global rdb
-  if rdb is None:
-    return "Redis host not set."
+  rdb = server.config["RDB"]
   counter = int(rdb.get("counter"))
   return f"{counter=}\nIncrement it by visiting `/increment`."
 
@@ -34,7 +30,6 @@ def index():
 def main(argv: Sequence[str]) -> None:
   del argv
 
-  global rdb
   rdb = redis.Redis(host=redis_host.value, decode_responses=True)
 
   while True:
@@ -45,9 +40,10 @@ def main(argv: Sequence[str]) -> None:
     except redis.exceptions.ConnectionError:
       time.sleep(1)
 
+  server.config["RDB"] = rdb
   rdb.set("counter", 0)
 
-  bottle.run(server, host="0.0.0.0", port=8080, debug=False)
+  server.run(host="0.0.0.0", port=8080, debug=False)
 
 
 if __name__ == "__main__":
