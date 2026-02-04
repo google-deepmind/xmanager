661f0ae9d19c08a884e881a4dc9f1f7f8db20576
Alex Iaru <alexiaru@google.com>
2026-02-04 09:02:13 -0800

    Add help command for third_party CLI.
    PiperOrigin-RevId: 865433016
    Change-Id: I31fee0652216828997b7f5c6e54307750198af9f
    GitOrigin-RevId: 7de5bcb24e06e7c3dcb10e0717b2f7fb6b5384f0

--- xmanager/cli/cli.py
+++ xmanager/cli/cli.py
@@ -16,7 +16,9 @@
 import errno
 import importlib
 import os
+import shutil
 import sys
+import textwrap
 
 from absl import app
 
@@ -24,44 +26,81 @@ _DEFAULT_ZONE = 'us-west1-b'
 _DEFAULT_CLUSTER_NAME = 'xmanager-via-caliban'
 
 
+def _help_command(argv):
+  """Prints help message."""
+  if len(argv) != 2:
+    raise app.UsageError('help command takes no arguments')
+  try:
+    width = shutil.get_terminal_size().columns
+  except OSError:
+    width = 80
+  print('usage: xmanager {launch,cluster} ...')
+  print()
+  opts = {
+      'launch': 'Launches an experiment on XManager.',
+      'cluster': (
+         'Creates or deletes a GKE cluster for use with xm_local.'
+      ),
+  }
+  for k, v in opts.items():
+    wrapper = textwrap.TextWrapper(
+        width=width, initial_indent=f'{k:<10}', subsequent_indent=' ' * 10
+    )
+    print(wrapper.fill(v))
+
+
+def _launch_command(argv):
+  """Launches an experiment using XManager."""
+  launch_script = argv[2]
+  if not os.path.exists(launch_script):
+    raise OSError(errno.ENOENT, f'File not found: {launch_script}')
+  sys.path.insert(0, os.path.abspath(os.path.dirname(launch_script)))
+  launch_module, _ = os.path.splitext(os.path.basename(launch_script))
+  m = importlib.import_module(launch_module)
+  sys.path.pop(0)
+  argv = [
+      launch_script,
+      '--xm_launch_script={}'.format(launch_script),
+  ] + argv[3:]
+  app.run(m.main, argv=argv)
+
+
+def _cluster_command(argv):
+  """Creates or deletes a GKE cluster for use with xm_local."""
+  caliban_gke = importlib.import_module('caliban.platform.gke.cli')
+  caliban_gke_types = importlib.import_module('caliban.platform.gke.types')
+  subcmd = argv[2]
+  args = {
+      'dry_run': False,
+      'cluster_name': _DEFAULT_CLUSTER_NAME,
+      'zone': _DEFAULT_ZONE,
+      'release_channel': caliban_gke_types.ReleaseChannel.REGULAR,
+      'single_zone': True,
+  }
+  if subcmd == 'create':
+    caliban_gke._cluster_create(args)  # pylint: disable=protected-access
+  elif subcmd == 'delete':
+    caliban_gke._cluster_delete(args)  # pylint: disable=protected-access
+  else:
+    raise app.UsageError(
+        f'Subcommand `cluster {subcmd}` is not a supported subcommand'
+    )
+
+
 def main(argv):
   if len(argv) < 3:
-    raise app.UsageError('There must be at least 2 command-line arguments')
-  cmd = argv[1]
-  if cmd == 'launch':
-    launch_script = argv[2]
-    if not os.path.exists(launch_script):
-      raise OSError(errno.ENOENT, f'File not found: {launch_script}')
-    sys.path.insert(0, os.path.abspath(os.path.dirname(launch_script)))
-    launch_module, _ = os.path.splitext(os.path.basename(launch_script))
-    m = importlib.import_module(launch_module)
-    sys.path.pop(0)
-    argv = [
-        launch_script,
-        '--xm_launch_script={}'.format(launch_script),
-    ] + argv[3:]
-    app.run(m.main, argv=argv)
-  elif cmd == 'cluster':
-    caliban_gke = importlib.import_module('caliban.platform.gke.cli')
-    caliban_gke_types = importlib.import_module('caliban.platform.gke.types')
-    subcmd = argv[2]
-    args = {
-        'dry_run': False,
-        'cluster_name': _DEFAULT_CLUSTER_NAME,
-        'zone': _DEFAULT_ZONE,
-        'release_channel': caliban_gke_types.ReleaseChannel.REGULAR,
-        'single_zone': True,
-    }
-    if subcmd == 'create':
-      caliban_gke._cluster_create(args)  # pylint: disable=protected-access
-    elif subcmd == 'delete':
-      caliban_gke._cluster_delete(args)  # pylint: disable=protected-access
+    if argv[1] == 'help':
+      _help_command(argv)
     else:
-      raise app.UsageError(
-          f'Subcommand `{cmd} {subcmd}` is not a supported subcommand'
-      )
+      raise app.UsageError('There must be at least 2 command-line arguments')
   else:
-    raise app.UsageError(f'Command `{cmd}` is not a supported command')
+    cmd = argv[1]
+    if cmd == 'launch':
+      _launch_command(argv)
+    elif cmd == 'cluster':
+      _cluster_command(argv)
+    else:
+      raise app.UsageError(f'Command `{cmd}` is not a supported command')
 
 
 def entrypoint():
